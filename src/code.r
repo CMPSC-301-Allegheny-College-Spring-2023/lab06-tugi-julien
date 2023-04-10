5b632320b05249f4e893f87018b2c490c134b091
tuduun <gantulga01@allegheny.edu>
2023-04-10 14:49:40 -0400

    Add male and female information

--- src/code.r
+++ src/code.r
@@ -6,8 +6,8 @@
 
 # Names: 
 print("The names of this group are the following.")
-print("Add Your Name Here")
-print("Add Your Name Here")
+print("Tuguldur")
+print("Julien")
 print("Add Your Name Here")
 print("Add Your Name Here")
 
@@ -36,21 +36,71 @@ cat("\014") # clear the console
 # you will need to install the dslabs library once.
 # install.packages("dslabs")
 
+library(dslabs)
 library(dplyr)
 library(tidyverse)
+library(ggplot2)
 
-##### Begin by loading your csv file.
-fname <- file.choose()
-data <- read.table(file.choose(), header = TRUE, sep = ",")
 
-# View the data set.
+dat <- read.table(file.choose(), header = T, sep = ",")
+dat1 <- read.table(file.choose(), header = T, sep = ",")
 
-View(data)
 
-# Add your code here to address the four main research questions which are noted in the README.md file. 
+# view the data set.
 
-# TODO
+# dat
 
-#######################################################
+df_transposed <- t(dat)
+df_transposed1 <- t(dat1)
 
-# Note: Did leave your name to the top of this script?
+
+df_transposed <- as.data.frame(df_transposed)
+df_transposed1 <- as.data.frame(df_transposed1)
+
+
+colnames(df_transposed) <- df_transposed[1,]
+colnames(df_transposed1) <- df_transposed1[1,]
+
+
+
+colnames(df_transposed)[3] <- "UK, Unemployment with advanced education, female (% of female labor force with advanced education)"
+colnames(df_transposed)[4] <- "US, Unemployment with advanced education, female (% of female labor force with advanced education)"
+
+colnames(df_transposed1)[3] <- "UK, Unemployment with advanced education, male (% of male labor force with advanced education)"
+colnames(df_transposed1)[4] <- "US, Unemployment with advanced education, male (% of male labor force with advanced education)"
+
+subset_UK_unemp <- df_transposed$`UK, Unemployment with advanced education, female (% of female labor force with advanced education)`[44:65]
+subset_UK_unemp <- as.data.frame(subset_UK_unemp)
+
+subset_UK_unemp1 <- df_transposed1$`UK, Unemployment with advanced education, male (% of male labor force with advanced education)`[44:65]
+subset_UK_unemp1 <- as.data.frame(subset_UK_unemp1)
+
+
+subset_US_unemp <- df_transposed$`US, Unemployment with advanced education, female (% of female labor force with advanced education)`[44:65]
+subset_US_unemp <- as.data.frame(subset_US_unemp)
+
+subset_US_unemp1 <- df_transposed1$`US, Unemployment with advanced education, male (% of male labor force with advanced education)`[44:65]
+subset_US_unemp1 <- as.data.frame(subset_US_unemp1)
+
+combined_subset <- cbind(subset_US_unemp, subset_UK_unemp)
+combined_subset1 <- cbind(subset_US_unemp1, subset_UK_unemp1)
+
+
+# add a new column called "years"
+combined_subset$years <- c("1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010","2011", "2012", "2013", "2014","2015","2016","2017","2018", "2019")
+combined_subset1$years <- c("1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010","2011", "2012", "2013", "2014","2015","2016","2017","2018", "2019")
+
+
+ggplot() +
+  geom_point(data = combined_subset, aes(x = years, y = subset_US_unemp), color = "red") +
+  geom_point(data = combined_subset, aes(x = years, y = subset_UK_unemp), color = "blue") +
+  xlab("Years") +
+  ylab("Unemployment with advanced education, female (% of female labor force with advanced education)") +
+  ggtitle("Women's unemployment rate in the U.S and U.K")
+
+ggplot() +
+  geom_point(data = combined_subset1, aes(x = years, y = subset_US_unemp1), color = "red") +
+  geom_point(data = combined_subset1, aes(x = years, y = subset_UK_unemp1), color = "blue") +
+  xlab("Years") +
+  ylab("Unemployment with advanced education, male (% of male labor force with advanced education)") +
+  ggtitle("Men's unemployment rate in the U.S and U.K")
